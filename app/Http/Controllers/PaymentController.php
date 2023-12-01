f9ca6fa97cfe4dbe8b41634d459198d22d3e1cc8
Tanima-062 <tyousuf141062@bscse.uiu.ac.bd>
2023-12-01 17:42:28 +0600

    :pencil2: Add: Files

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Payment;
+
+class PaymentController extends Controller
+{
+    public function save(Request $request)
+    {
+        $status = $this->getMockResponse($request);
+
+        $transaction_id = time().uniqid(mt_rand(),true);
+
+        $payment = new Payment();
+        $payment->user_id = 1;
+        $payment->transaction_id = $transaction_id;
+        $payment->amount = $request->amount;
+        $payment->status = $status;
+        $payment->save();
+
+        return response(['message' => 'Data has been stored successfully'])->header('Cache-Control', 'nocache, no-store');
+        
+
+    }
+
+    public function getMockResponse(Request $request){
+        $request->headers->set('X-Mock-Status', true);
+        if($request->header('X-Mock-Status')){
+            return $request->header('X-Mock-Status') == true ? 'accepted' : 'failed';
+        }
+    }
+
+    public function update(Request $request){
+        Payment::update(['status'=>$request->status])->where(['transaction_id' => $request->transaction_id, 'user_id' => 1]);
+         return response(['message' => 'Data has been updated successfully']);
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,10 @@
+===================To Start Docker Desktop===================
+1. systemctl --user start docker-desktop
+
+======================= to up project on docker ================
+
+1. docker-compose up -d --build
+2. docker-compose exec app bash
+3. chmod 777 storage/ -R
+4. chmod 777 bootstrap/cache -R
+5. php artisan key:generate
